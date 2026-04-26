1783e24ea42a5e11f492181aebf754a0c1f331f8
Bod <abdelrahman.ali.dev@gmail.com>
2026-04-26 17:28:01 +0300

    Update package version from 2.0.0 to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vizods",
-    version="2.0.0",
+    version="1.0.0",
     author="Abdelrahman Ali",
     author_email="abdelrahman.ali.dev@gmail.com",
     description="A Python library for visualizing Data Structures and Algorithms with images and videos",
