9c1043edcd1cccc5cdb373a9a568091548a8feea
Bod <abdelrahman.ali.dev@gmail.com>
2026-04-26 16:22:19 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vizods",
-    version="1.0.1",
+    version="1.0.2",
     author="Abdelrahman Ali",
     author_email="abdelrahman.ali.dev@gmail.com",
     description="A Python library for visualizing Data Structures and Algorithms with images and videos",
